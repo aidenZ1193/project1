3886cbf9514429b9205f03d4e426f952a67f1a81
aidenZ1193 <aidenz1193@gmail.com>
2018-10-04 21:45:52 -0700

    moving to insertRecord need to figure out the structure before coding

--- rbf/rbfm.cc
+++ rbf/rbfm.cc
@@ -42,11 +42,23 @@ RC RecordBasedFileManager::destroyFile(const string &fileName) {
 }
 
 RC RecordBasedFileManager::openFile(const string &fileName, FileHandle &fileHandle) {
-    return -1;
+
+    // if fileHandle is valid:
+    if(fileHandle.getFilePinter() != NULL || fileHandle.getFileName() != "")
+        return -1;
+
+    if(_pf_manager->openFile(fileName, fileHandle) != 0)
+        return -1;
+
+    return 0;
 }
 
 RC RecordBasedFileManager::closeFile(FileHandle &fileHandle) {
-    return -1;
+
+    if(_pf_manager->closeFile(fileHandle) != 0)
+        return -1;
+        
+    return 0;
 }
 
 RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const void *data, RID &rid) {
